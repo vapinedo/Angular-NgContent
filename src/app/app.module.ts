6f10353b798154e3aa7d4cb50a391a429f9e5dbd
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-23 09:14:42 -0500

    [update] refactor whole app to use ng-content directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserAdminComponent } from './components/user-admin/user-admin.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserAdminComponent
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
